619bf174e48440c37a263cdb7d5d9408a89e1357
binit <binitgupta.1711@gmail.com>
2025-07-08 11:13:30 +0530

    Fix Vercel deployment: remove base path and add proper routing config

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ function App() {
           <div className="flex justify-between items-center h-16">
             {/* Logo */}
             <div className="flex items-center space-x-2">
-              <img src="/hospit-all/images/image.png" alt="Hospit-All Logo" className="h-10 w-10 object-contain" />
+              <img src="/images/image.png" alt="Hospit-All Logo" className="h-10 w-10 object-contain" />
               <span className="font-bold text-xl text-gray-800">Hospit-All</span>
             </div>
 
@@ -101,7 +101,7 @@ function App() {
           <div className="text-center">
             <div className="mb-8">
               <img 
-                src="/hospit-all/images/image.png" 
+                src="/images/image.png" 
                 alt="Hospit-All Logo" 
                 className="h-16 w-16 object-contain mx-auto mb-4"
               />
